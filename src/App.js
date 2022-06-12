3d17346f71d3778dbeea5d2186802df48d986b16
sarankumarunifo <89625600+sarankumarunifo@users.noreply.github.com>
2022-06-12 18:54:09 +0530

    inital commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import Home from './Components/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Router>
+          <Routes>
+            <Route exact path="/" element ={<Home/>} />
+          </Routes>
+        </Router>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
